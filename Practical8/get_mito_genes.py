7683bf59dc7ccfc21a809970bfb0d14e4a31cbdd
xiangjunle <junle.23@intl.zju.edu.cn>
2024-07-20 22:44:19 +0800

    x

--- Practical8/get_mito_genes.py
+++ Practical8/get_mito_genes.py
@@ -1,4 +1,3 @@
-# get_mito_genes.py
 
 def extract_mitochondrial_genes(fasta_file, output_file):
     with open(fasta_file, 'r') as file:
@@ -8,17 +7,16 @@ def extract_mitochondrial_genes(fasta_file, output_file):
         current_gene = None
         for line in lines:
             if line.startswith('>'):
-                # 检查是否是线粒体基因
+                # Check if it is a mitochondrial gene
                 if 'Mito' in line:
-                    # 提取基因名称
+                    # Extract gene names
                     gene_name = line.strip()[1:].split()[0]
                     current_gene = gene_name
                     outfile.write(f">{gene_name}\n")
                 else:
-                    # 不是线粒体基因，重置当前基因
+                    # Not a mitochondrial gene, reset the current gene
                     current_gene = None
             elif current_gene:
-                # 写入当前基因的序列
                 outfile.write(line)
 
 if __name__ == "__main__":
